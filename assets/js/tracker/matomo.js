9da9d915f9ad4b207eeb02cfa86c0af4016d797d
Hannes <50320115+hienous@users.noreply.github.com>
2020-11-05 13:54:04 +0200

    added expiration time to matomo cookies

--- assets/js/tracker/matomo.js
+++ assets/js/tracker/matomo.js
@@ -1,11 +1,15 @@
   var _paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  /*
+  * tracker methods like "setCustomDimension" should be called before "trackPageView",
+  * "setVisitorCookieTimeout" - set expiration time for _pk_id cookie in seconds, currently 90 days.
+  */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
     var u="https://testivaraamo.turku.fi:8003/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '4']);
+    _paq.push(['setVisitorCookieTimeout', '7776000']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
